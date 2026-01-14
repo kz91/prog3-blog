61b10dfbd3e7ac9aa736e95b1404ae40c0ee7ada
kz91 <kznyan91@gmail.com>
2026-01-14 23:16:15 +0900

    Fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
     // Local development config
     reactStrictMode: true,
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
     experimental: {
         serverActions: {
             bodySizeLimit: '10mb',
